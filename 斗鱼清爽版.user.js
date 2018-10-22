ec191a17fd505741850b079518f9e887187c55d1
z.houbin <z.houbin@foxmail.com>
2018-10-22 19:45:36 +0800

    更新斗鱼清爽版,适配网站更新

--- 斗鱼清爽版.user.js
+++ 斗鱼清爽版.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         斗鱼清爽版
 // @namespace    http://z.houbin.site/douyu/
-// @version      0.2
+// @version      0.5
 // @description  斗鱼清爽版,去送礼通知,二维码,聊天顶部贵族,系统通知,礼物通知,竞猜,鱼吧,推荐,聊天样式统一,一些小按钮
 // @author       z.houbin
 // @match        *://www.douyu.com/*
@@ -9,66 +9,85 @@
 // ==/UserScript==
 
 (function () {
-    var css = '.giftbatter-box{display:none !important;}';
-    css += '.peck{display:none !important;}';
-    css += '#js-fans-rank{display:none !important;height:0px !important}';
-    css += '#js-chat-cont{top:0px !important;}';
-    css += '#js-stats-and-actions{display:none !important}';
-    css += '.giftbatter-noble-enter{display:none !important}';
-    css += '.sq-ad{display:none !important}';
-    css += '.no-login{display:none !important}';
-    css += '.guess-main-panel{display:none !important}';
-    css += '.PlayerSub{display:none !important}';
-    css += '.wm_ladder-nav{display:none !important}';
-    css += 'div[data-component-id*="room"]{height:100% !important}';
-    css += '.PlayerCaseSub-Main{top:0px !important;}';
-    css += '.status-low-enter{display:none !important;}';
-    css += '.user-noble{display:none !important;}';
-    css += '.firstpay-award-icon{display:none !important;}';
-    css += '.fans-icon-name{display:none !important;}';//粉丝牌
-    css += '.chat-icon-pad{margin:0 !important;}';//粉丝牌
-    css += '.motorcade-icon{display: none !important;margin:0 !important;}';//车
-    css += '.o-download{display:none !important;}';//客户端
-    css += '.catagory-order-container{display:none !important;}';//本周排名
-    css += '.hot-box{display:none !important;}';//热度介绍
-    css += '#share-content{display:none !important;}';//分享
-    css += '#qrcode-content{display:none !important;}';//客户端二维码
-    css += 'div[class*="broadcastDiv"]{display:none !important;}';//广播
-    css += 'div[class*="customBc"]{display:none !important;}';//广播
-    css += 'div[class*="recommendApp"]{display:none !important;}';//推荐app
-    css += 'div[class*="recommendAD"]{display:none !important;}';//推荐广告
-    css += '.QRcode{display:none !important;}';//二维码
-    css += '.starsgathered{display:none !important;}';//聚星榜
-    css += '.icon-role{margin-right:10px !important;}';//聚星榜
-    css += '.high-notice-new{display:none !important;}';//聊天通知
-    css += '.normal-notice-new{display:none !important;}';//聊天通知
-    css += '.chat-msg-item{color:#000 !important;}';//聊天文字颜色
+    var css = '.layout-Player-rank{display:none !important;height:0 !important}';//贵族榜
+    css += '.fans-rank{display:none !important;height:0 !important}';//贵族榜2
+    css += '.layout-Player-barrage{top:2px !important;}';//弹幕
+    css += '.js-chat-cont{top:2px !important;}';//弹幕
+    css += '.chat-cont{top:2px !important;}';//弹幕
+    css += '.Barrage-chat-ad{display:none !important;}';//聊天广告
+    css += '.js-fans-dysclick{display:none !important;}';//粉丝牌
+    css += '.SignBaseComponent-sign-ad{display:none !important;}';//提示登录广告
+    css += '.sq-ad{display:none !important;}';//提示登录广告
+    css += '.layout-Player-toolbar{display:none !important;height:0px !important}';//视频下方礼物信息
+    css += '.stats-and-actions{display:none !important;height:0px !important}';//视频下方礼物信息
+    css += '.sys-tips{display:none !important;}';//系统提示
+    css += '.Title-roomOtherBottom{display:none !important;}';//分享/客户端观看
+    css += '.sq-wrap{display:none !important;}';//分享/客户端观看
+    css += '#js-header > div > div > div.Header-left > div > ul > li:nth-child(5){display:none !important;}';//顶部游戏
+    css += '.funny{display:none !important;}';//顶部游戏
+    css += '#js-header > div > div > div.Header-left > div > ul > li:nth-child(6){display:none !important;}';//顶部鱼吧
+    css += '.yuba{display:none !important;}';//顶部鱼吧
+    css += '.DropPane-ad{display:none !important;height:0px !important}';//历史下拉广告
+    css += '.his-sign-cont{display:none !important;height:0px !important}';//历史下拉广告
+    css += '.f-sign-cont{display:none !important;height:0px !important}';//关注下拉广告
+    css += '.assort-ad{display:none !important;height:0px !important}';//分类下拉广告
     css += 'div[data-type*="dyrec"]{display:none !important;}';//首页斗鱼推广
     css += 'div[data-type*="sign"]{display:none !important;}';//首页广告
-    //css += 'a[href*="g.wan"]{display:none !important;}';//游戏广告
-    css += '.GameDownload{display:none !important;}';//游戏下载
-    css += '.impress-add{display:none !important;}';//+印象标签
-    css += '.assort-ad{display:none !important;}';//分类下拉
-    css += '.his-sign-cont{display:none !important;}';//关注下拉广告
-    css += '.f-sign-cont{display:none !important;}';//历史下拉广告
-    css += '.rank-video-ad{display:none !important;}';//历史下拉广告
-    css += '.lol-ad{display:none !important;}';//历史下拉广告
-    //css += '.bglink{display:none !important;}';//首页左右
+    css += '.layout-Bottom{display:none !important;}';//视频播放底部(互动竞猜/鱼吧)
+    css += '.QRcode{display:none !important;}';//视频右侧二维码
+    css += '.channelGame{display:none !important;}';//左侧页游中心
+    css += '#__h5player > div:nth-child(11){display:none !important;}';//亲密互动
+    css += '.chat-more-message{display:none !important;}';//聊天框多余信息
+    css += '#treasure{display:none !important;}';//宝藏
+    css += '.fans-icon-name{display:none !important;}';//粉丝牌
+    css += '.motorcade-icon{display:none !important;}';//粉丝牌
+    css += '.chat-msg-item{color:#000 !important;}';//聊天文字颜色
+    css += '.high-notice-new{display:none !important;}';//聊天通知
+    css += '.normal-notice-new{display:none !important;}';//聊天通知
+    css += '.chat-ad{display:none !important;}';//聊天广告(签到)
+    css += '.chat-icon-pad{margin:0 !important;}';//聊天等级对齐
+    css += '.lol-ad{display:none !important;}';//聊天等级对齐
 
-    var chat = document.getElementsByClassName('chat-cont-wrap')[0];
-    if (chat !== undefined) {
-        chat.addEventListener("DOMNodeInserted", function (e) {
-            var node = e.target;
-            if (node.tagName === 'LI') {
-                if (node.innerText.indexOf('欢迎来到本直播间') !== -1) {
-                    node.parentElement.removeChild(node);
+
+
+    setTimeout(function () {
+        //进入直播间提示
+        var barrage = document.getElementsByClassName('Barrage-list')[0];
+        if (barrage !== undefined) {
+            barrage.addEventListener("DOMNodeInserted", function (e) {
+                var node = e.target;
+                if (node.tagName === 'LI') {
+                    if (node.innerText.indexOf('欢迎来到本直播间') !== -1) {
+                        node.parentElement.removeChild(node);
+                    }
+                    if (node.innerText.indexOf('赠送给主播') !== -1) {
+                        node.parentElement.removeChild(node);
+                    }
                 }
-                if (node.innerText.indexOf('赠送给主播') !== -1) {
-                    node.parentElement.removeChild(node);
+            });
+        }
+        //进入直播间提示
+        var chat = document.getElementsByClassName('chat-cont-wrap')[0];
+        if (chat !== undefined) {
+            chat.addEventListener("DOMNodeInserted", function (e) {
+                var node = e.target;
+                if (node.tagName === 'LI') {
+                    if (node.innerText.indexOf('欢迎来到本直播间') !== -1) {
+                        node.parentElement.removeChild(node);
+                    }
+                    if (node.innerText.indexOf('赠送给主播') !== -1) {
+                        node.parentElement.removeChild(node);
+                    }
                 }
-            }
-        });
-    }
+            });
+        }
+        //屏蔽全部特效
+        var shie = document.getElementsByClassName('ShieldTool-listItem')[0];
+        if (shie.className.indexOf('is-noChecked') !== -1) {
+            shie.click();
+        }
+    }, 3000);
+
     loadStyle(css);
 
     function loadStyle(css) {
